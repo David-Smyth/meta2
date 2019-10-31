0a56409da62272e716a102f0cd40f67fa6932ef5
David Smyth <dsmyth@westlawn.org>
2019-10-30 17:01:08 -0700

    Make to_alphanumeric a method of String

---
+++ Part1/02_the_object_world/test_alphanumeric2.rb
@@ -0,0 +1,8 @@
+require 'test/unit'
+require './alphanumeric2'
+
+class ToAlphanumericTest < Test::Unit::TestCase
+  def test_strip_non_alphanumeric_characters
+    assert_equal '3 the Magic Number', '#3, the *Magic, Number*?'.to_alphanumeric
+  end
+end
